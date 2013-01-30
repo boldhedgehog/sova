4d61b45d8c1def2586487d40a54a6425c20aad31
Aleksandr Yegorov <boldhedgehog@gmail.com>
2013-01-30 15:48:57 +0200

    Added DB config file to ignore.

---
+++ inc/dbconfig.sample.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database connections main file
+ *
+*/
+
+
+/*****************************************************************************
+	main database connection config
+	
+*****************************************************************************/
+
+/* LOCAL DATABASE */
+   define('DB_SERVER', 'localhost');
+   define('DB_USER', 'user');
+   define('DB_PASS', 'password');
+   define('DB_NAME', 'dbname');
+
+$GLOBALS["DEFAULT_DBMS"] = "mysql";
